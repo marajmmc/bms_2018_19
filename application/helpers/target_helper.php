e25b1e43001fc5098163fcb0652f8d0df24adcbc
mahmud369 <mahmud369@gmail.com>
2020-03-24 09:49:03 +0600

    Delete function modified.

--- application/helpers/target_helper.php
+++ application/helpers/target_helper.php
@@ -19,8 +19,103 @@ class Target_helper
             Target_helper::$update_success_status[] = FALSE;
         }
     }
+    public static function delete_target_tree($task)
+    {
+        $CI = & get_instance();
+        $user = User_helper::get_user();
+        $time = time();
+        $item_id = $CI->input->post('id');
+        $item = $CI->input->post('item');
+        $get_location_relation_ids=Target_helper::get_location_relation_ids($task,$item_id);
 
-    public static function delete_target_tree($task){
+        $delete_array = array
+        (
+            'status' => $CI->config->item('system_status_active'),
+            'status_forward' => $CI->config->item('system_status_pending'),
+            'remarks_delete' => $item['remarks_delete'],
+            'date_deleted' => $time,
+            'user_deleted' => $user->user_id
+        );
+        $delete_parent_array = array
+        (
+            'status' => $item['status'],
+            'remarks_delete_parent' => $item['remarks_delete'],
+            'date_deleted_parent' => $time,
+            'user_deleted_parent' => $user->user_id
+        );
+        if($task=='hq')
+        {
+            $CI->db->set('revision_count_delete', 'revision_count_delete+1', FALSE);
+            Query_helper::update($CI->config->item('table_bms_target_hq'), $delete_array, array("id =".$item_id), FALSE);
+            Query_helper::update($CI->config->item('table_bms_target_division'), $delete_parent_array, array("target_hq_id =".$item_id), FALSE);
+            if(sizeof($get_location_relation_ids['zone'])>0)
+            {
+                $parent_id = implode(', ', $get_location_relation_ids['zone']);
+                Query_helper::update($CI->config->item('table_bms_target_zone'), $delete_parent_array, array("id IN (" . $parent_id . ")"), FALSE);
+                Query_helper::update($CI->config->item('table_bms_target_territory'), $delete_parent_array, array("target_zone_id IN (" . $parent_id . ")"), FALSE);
+            }
+        }
+        elseif($task=='division')
+        {
+            $CI->db->set('revision_count_delete', 'revision_count_delete+1', FALSE);
+            Query_helper::update($CI->config->item('table_bms_target_division'), $delete_array, array("id =".$item_id), FALSE);
+
+            Query_helper::update($CI->config->item('table_bms_target_zone'), $delete_parent_array, array("target_division_id =".$item_id), FALSE);
+            if(sizeof($get_location_relation_ids['zone'])>0)
+            {
+                $parent_id = implode(', ', $get_location_relation_ids['zone']);
+                Query_helper::update($CI->config->item('table_bms_target_territory'), $delete_parent_array, array("target_zone_id IN (" . $parent_id . ")"), FALSE);
+            }
+        }
+        elseif($task=='zone')
+        {
+            $CI->db->set('revision_count_delete', 'revision_count_delete+1', FALSE);
+            Query_helper::update($CI->config->item('table_bms_target_zone'), $delete_array, array("id =".$item_id), FALSE);
+            Query_helper::update($CI->config->item('table_bms_target_zone'), $delete_parent_array, array("target_zone_id =".$item_id), FALSE);
+        }
+    }
+    public static function get_location_relation_ids($task, $item_id)
+    {
+        $CI = & get_instance();
+        $user = User_helper::get_user();
+        $item_ids['hq']=array();
+        $item_ids['division']=array();
+        $item_ids['zone']=array();
+        if($task=='hq')
+        {
+            $results = Query_helper::get_info($CI->config->item('table_bms_target_division'), array('*'), array('target_hq_id =' . $item_id, "status = '".$CI->config->item('system_status_active')."'"));
+            foreach($results as $result)
+            {
+                $item_ids['division'][$result['id']]=$result['id'];
+            }
+            if(sizeof($item_ids['division'])>0)
+            {
+                $parent_id = implode(', ', $item_ids['division']);
+                $results = Query_helper::get_info($CI->config->item('table_bms_target_zone'), array('*'), array('target_division_id IN (' . $parent_id .')', "status = '".$CI->config->item('system_status_active')."'"));
+                foreach($results as $result)
+                {
+                    $item_ids['zone'][$result['id']]=$result['id'];
+                }
+            }
+
+        }
+        elseif($task=='division')
+        {
+            $results = Query_helper::get_info($CI->config->item('table_bms_target_zone'), array('*'), array('target_division_id =' . $item_id, "status = '".$CI->config->item('system_status_active')."'"));
+            foreach($results as $result)
+            {
+                $item_ids['zone'][$result['id']]=$result['id'];
+            }
+        }
+        elseif($task=='zone')
+        {
+
+        }
+        return $item_ids;
+    }
+
+    /*public static function delete_target_tree($task)
+    {
         $CI = & get_instance();
         $user = User_helper::get_user();
         $time = time();
@@ -150,37 +245,17 @@ class Target_helper
             }
         }
     }
-
+*/
     public static function get_delete_info($params)
     {
         $CI = & get_instance();
         $user = User_helper::get_user();
 
-        // Main Table variables
-        $main_table = $params['main_table'];
         // Details Table variables
         $details_table = $params['details_table'];
         $location_table = $params['location_table'];
         $location_id_field = $params['location_id_field'];
         $foreign_key = $params['foreign_key'];
-
-        // Main Table Query
-        $CI->db->from($main_table);
-        $CI->db->select('*');
-        $CI->db->where("user_deleted", $user->user_id);
-        $CI->db->where("revision_count_delete > ", 0);
-        if(isset($params['year']) && ($params['year'] > 0)){
-            $CI->db->where("year", $params['year']);
-        }
-        if(isset($params['month']) && ($params['month'] > 0)){
-            $CI->db->where("month", $params['month']);
-        }
-        $results = $CI->db->get()->result_array();
-
-        $ids = array(0);
-        foreach($results as $result){
-            $ids[] = $result['id']; // Collecting ids for Query in Details Table
-        }
 
         // Validation Check??
 
@@ -192,9 +267,18 @@ class Target_helper
         $CI->db->select('location.name');
 
         $CI->db->where('details.status', $CI->config->item('system_status_delete'));
-        $CI->db->where('user_deleted_parent', $user->user_id);
-        $CI->db->where_in("details.{$foreign_key}", $ids);
+        if ($user->user_group != $CI->config->item('USER_GROUP_SUPER')) // If not SuperAdmin, Then user can only access own Deleted Item.
+        {
+            $CI->db->where('user_deleted_parent', $user->user_id);
+        }
+        if(isset($params['year']) && ($params['year'] > 0)){
+            $CI->db->where("details.year", $params['year']);
+        }
+        if(isset($params['month']) && ($params['month'] > 0)){
+            $CI->db->where("details.month", $params['month']);
+        }
         $detail_results = $CI->db->get()->result_array();
+        //echo $CI->db->last_query();
 
         $user_ids = array();
         foreach($detail_results as $detail_result){
